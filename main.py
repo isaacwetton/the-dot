8fd7772e6c0fa40062516f55c68500a479704738
Isaac Wetton <isaac@wetton.net>
2021-09-30 13:33:20 +0100

    Create fullscreen game window and game loop
    The window is fullscreen, and runs at 144fps.

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ pygame.init()
 
 # Create pi constant from math
 PI = math.pi
+
+# Create fullscreen game window
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+# Set window title
+pygame.display.set_caption("The Dot")
+
+# Initialise quit condition
+finished = False
+
+# Create clock
+clock = pygame.time.Clock()
+
+# --- MAIN PROGRAM LOOP ---
+
+while not finished:
+
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+    # Limit game to 144fps
+    clock.tick(144)

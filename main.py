a525670772b245a7fe87b422e9d5ab013154a85e
Isaac Wetton <isaac@wetton.net>
2021-09-30 15:26:23 +0100

    Added colours, changed background to DARK_GREY, created pygame Info() object

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import math
 pygame.init()
 
 # Define any colours used
+DARK_GREY = (85, 85, 85)
+WHITE = (255, 255, 255)
 
 # Create pi constant from math
 PI = math.pi
 
+# Create pygame info object
+pygameInfo = pygame.display.Info()
+
 # Create fullscreen game window
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+# screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((pygameInfo.current_w, pygameInfo.current_h))
 
 # Set window title
 pygame.display.set_caption("The Dot")
@@ -31,5 +37,13 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
 
+    # Clear the screen to DARK_GREY
+    screen.fill(DARK_GREY)
+    
+    pygame.display.flip()
+
     # Limit game to 60fps
     clock.tick(60)
+
+pygame.quit()
+quit()

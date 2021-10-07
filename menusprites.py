8d76ab2cb69c89c6774cf903788a8cfa4f13a699
Isaac Wetton <isaac@wetton.net>
2021-10-07 23:28:05 +0100

    Added MainMenuPlay class to menusprites.py, for the main menu play button

--- menusprites.py
+++ menusprites.py
@@ -15,7 +15,24 @@ class MainMenuTitle(pygame.sprite.Sprite):
         super().__init__()
 
         # Load the main menu image and corresponding rectangle dimensions
-        self.image = pygame.image.load("TheDotTitle.png").convert_alpha()
+        self.image = pygame.image.load("TheDotTitle.png").convert()
+        self.image = pygame.transform.scale(self.image, size)
+        self.rect = self.image.get_rect()
+        self.rect.x = xpos
+        self.rect.y = ypos
+
+
+class MainMenuPlay(pygame.sprite.Sprite):
+    """This is the sprite for the main menu's play button. If the user clicks on this sprite, the game will start."""
+
+    def __init__(self, xpos, ypos, size):
+        """Constructor.... """
+
+        # Call pygame.sprite.Sprite constructor
+        super().__init__()
+
+        # Load the main menu image and corresponding rectangle dimensions
+        self.image = pygame.image.load("TheDotPlay.png").convert()
         self.image = pygame.transform.scale(self.image, size)
         self.rect = self.image.get_rect()
         self.rect.x = xpos
